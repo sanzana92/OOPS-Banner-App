b6a52c2edf00de3a7913573d57ea0f890866daf3
Sanzana Rreddy <sa5648@srmist.edu.in>
2026-03-01 22:01:21 +0530

    UC3: Refactor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,47 @@
-public static void main(String[] args) {
+public class OOPSBannerApp {
 
-    System.out.println("  *****  " + "  *****  " + " ******* " + "  *****  ");
-    System.out.println(" **   ** " + " **   ** " + " **    **" + " **    ** ");
-    System.out.println(" **   ** " + " **   ** " + " **    **" + " **       ");
-    System.out.println(" **   ** " + " **   ** " + " ******* " + "  *****   ");
-    System.out.println(" **   ** " + " **   ** " + " **       " + "       ** ");
-    System.out.println(" **   ** " + " **   ** " + " **       " + " **    ** ");
-    System.out.println("  *****  " + "  *****  " + " **       " + "  *****   ");
+    public static void main(String[] args) {
 
+        System.out.println(String.join("",
+                "  *****  ",
+                "  *****  ",
+                "  ******* ",
+                "  *****  "));
+
+        System.out.println(String.join("",
+                " **   ** ",
+                " **   ** ",
+                " **    **",
+                " **    **"));
+
+        System.out.println(String.join("",
+                " **   ** ",
+                " **   ** ",
+                " **    **",
+                " **      "));
+
+        System.out.println(String.join("",
+                " **   ** ",
+                " **   ** ",
+                "  ******* ",
+                "  *****   "));
+
+        System.out.println(String.join("",
+                " **   ** ",
+                " **   ** ",
+                " **       ",
+                "      **  "));
+
+        System.out.println(String.join("",
+                " **   ** ",
+                " **   ** ",
+                " **       ",
+                " **    ** "));
+
+        System.out.println(String.join("",
+                "  *****  ",
+                "  *****  ",
+                " **       ",
+                "  *****   "));
+    }
 }
